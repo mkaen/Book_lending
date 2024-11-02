ddfe41fe1642f215b775b2de82fcf3209f0f7951
marguskaen <margus.kaen@gmail.com>
2024-11-02 18:31:59 +0200

    added automatic logout() after each test

--- tests/authorization.py
+++ tests/authorization.py
@@ -5,16 +5,10 @@ def login(client, username: str):
         'username': username,
         'password': '123456'
     }, follow_redirects=True)
-    # print(f"\nLogin status code: {response.status_code}")
-    # print(f"Is logged in: {current_user.is_authenticated}")
-    # with client.session_transaction() as session:
-    #     print(f"\nUser id in session: {session.get('_user_id')}")
     return response
 
 
 def logout(client):
     """Log out the user."""
     response = client.get('/logout', follow_redirects=True)
-    # with client.session_transaction() as session:
-    #     print(f"\nUser id in session: {session.get('_user_id')}")
     return response
