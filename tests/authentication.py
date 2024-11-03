eeb24004b8b5c5ccbd05b3b314f408756626ad7a
marguskaen <margus.kaen@gmail.com>
2024-11-03 11:00:12 +0200

    renamed authentication.py file

--- tests/authentication.py
+++ tests/authentication.py
@@ -1,11 +1,14 @@
 
 def login(client, username: str):
     """Log in the user."""
-    response = client.post('/login', data={
-        'username': username,
-        'password': '123456'
-    }, follow_redirects=True)
-    return response
+    try:
+        response = client.post('/login', data={
+            'username': username,
+            'password': '123456'
+        }, follow_redirects=True)
+        return response
+    except Exception as e:
+        return e
 
 
 def logout(client):
